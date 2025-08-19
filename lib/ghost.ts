76254ea65a9e6144ffc4f9e732d8db631f88dd71
mackzeru <mack.carter@softsatnd.com>
2025-08-19 13:18:14 +0300

    fix: add default Ghost API URL and key for local development The changes provide fallback values for Ghost API configuration to simplify local development setup. This avoids the need to set environment variables during initial development.

--- lib/ghost.ts
+++ lib/ghost.ts
@@ -5,8 +5,10 @@ import GhostContentAPI, {
 
 // Initialize Ghost API
 const api = new GhostContentAPI({
-  url: process.env.NEXT_PUBLIC_GHOST_API_URL ?? "",
-  key: process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY ?? "",
+  url: process.env.NEXT_PUBLIC_GHOST_API_URL ?? "http://192.168.43.62:2368",
+  key:
+    process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY ??
+    "d1430b72c0476d0b563776b904",
   version: "v5.0",
 });
 
